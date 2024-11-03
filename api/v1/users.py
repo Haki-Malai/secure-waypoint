a67e057e4e112ecb87b0d30b1181f477016ac0f8
Haki-Malai <hakimalai@outlook.com>
2024-11-03 20:45:48 +0200

    feat(api/v1): add version 1 (v1) API routes for health check and user operations using FastAPI framework.

---
+++ api/v1/users.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Depends
+
+from app.controllers import UserController
+from app.schemas.responses import UserResponse
+from core.factory import Factory
+
+users_router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@users_router.get("", response_model=list[UserResponse])
+async def get_users(
+    user_controller: UserController = Depends(Factory().get_user_controller),
+):
+    users = await user_controller.get_all()
+    return users
+
+
+@users_router.get("/{user_id}", response_model=UserResponse)
+async def get_user_by_id(
+    user_id: int,
+    user_controller: UserController = Depends(Factory().get_user_controller),
+):
+    user = await user_controller.get_by_id(user_id)
+    return user
+
+
+@users_router.get("/search", response_model=list[UserResponse])
+async def search_users_by_name(
+    query: str, user_controller: UserController = Depends(Factory().get_user_controller)
+):
+    users = await user_controller.search_by_name(query)
+    return users
