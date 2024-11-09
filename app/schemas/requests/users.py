b5bd2a303e63e6ca3fd60b16be1ef4aa0d9b8897
Haki-Malai <hakimalai@outlook.com>
2024-11-09 17:29:45 +0200

    ✨ (user.py): Add password hashing functionality for user passwords to improve security 📝 (users.py): Update user registration request schema to include password field 🔧 (password_handler.py): Add password hashing and verification methods for user passwords ⬆️ (pyproject.toml): Update passlib dependency version to ensure compatibility with password hashing functionality

--- app/schemas/requests/users.py
+++ app/schemas/requests/users.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, constr, field_validator
 
 class RegisterUserRequest(BaseModel):
     username: constr(min_length=3, max_length=64)
+    password: constr(min_length=8, max_length=64)
 
     @field_validator("username")
     def username_must_not_contain_special_characters(cls, v):
---
+++ core/security/__init__.py
@@ -0,0 +1,5 @@
+from .password_handler import password_handler
+
+__all__ = [
+    "password_handler",
+]
