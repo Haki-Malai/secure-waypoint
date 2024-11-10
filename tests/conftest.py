9a6ed05829de80e98a666ebf4fb5c647c800625a
Haki-Malai <hakimalai@outlook.com>
2024-11-10 12:34:00 +0200

    ⬆ (pyproject.toml): add pytest-asyncio dependency to support async testing 📝 (tests): add test files and fixtures for testing JWT handler and database sessions

---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+import asyncio
+import os
+from collections.abc import AsyncGenerator, Generator
+
+import pytest
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.models import Base
+
+TEST_DATABASE_URL = os.getenv("TEST_POSTGRES_URL")
+
+
+@pytest.fixture(scope="session")
+def event_loop(request) -> Generator:  # noqa: ARG001
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope="function")
+async def db_session() -> AsyncGenerator:
+    async_engine = create_async_engine(TEST_DATABASE_URL)
+    session = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
+
+    async with session() as s:
+        async with async_engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+        yield s
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        pass
+
+    await async_engine.dispose()
