54913b99a5ac7368d95de77a84c0b1ad57971185
Haki-Malai <hakimalai@outlook.com>
2024-11-11 14:03:00 +0200

    🔧 Add .env.example file with default environment variables for development setup ♻️ Refactor TEST_DATABASE_URL in conftest.py to use config module for SQLALCHEMY_DATABASE_URI and append '-test' suffix for testing purposes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import asyncio
-import os
 from collections.abc import AsyncGenerator, Generator
 
 import pytest
@@ -8,12 +7,13 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.models import Base
+from core.config import config
 
-TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
+TEST_DATABASE_URL = f"{str(config.SQLALCHEMY_DATABASE_URI)}-test"
 
 
 @pytest.fixture(scope="session")
-def event_loop(request) -> Generator:  # noqa: ARG001
+def event_loop(request: pytest.FixtureRequest) -> Generator:  # noqa: ARG001
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
