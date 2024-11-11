7f22874bd3cacd9a46353e3e655059d07521d679
Haki-Malai <hakimalai@outlook.com>
2024-11-11 13:44:27 +0200

    ✨ (users.py): Add UserPagination schema to handle pagination and filtering parameters for get_users endpoint 📝 (users.py): Update __init__.py to include UserPagination in the exported schemas 📝 (users.py): Add UserPagination schema to handle pagination and filtering parameters for get_users endpoint

--- app/schemas/requests/users.py
+++ app/schemas/requests/users.py
@@ -1,8 +1,10 @@
 import re
+from datetime import date
 
 from pydantic import BaseModel, constr, field_validator
 
 from app.models import Role
+from core.exceptions import BadRequestException
 
 
 class BaseUserRequest(BaseModel):
@@ -11,7 +13,7 @@ class BaseUserRequest(BaseModel):
     @field_validator("username")
     def username_must_not_contain_special_characters(cls, v):
         if v is not None and re.search(r"[^a-zA-Z0-9]", v):
-            raise ValueError("Username must not contain special characters")
+            raise BadRequestException("Username must not contain special characters")
         return v
 
 
@@ -23,3 +25,16 @@ class UpdateUserRequest(BaseUserRequest):
     username: constr(min_length=3, max_length=64) | None = None
     role: Role | None = None
     password: constr(min_length=8, max_length=64) | None = None
+
+
+class UserPagination(BaseModel):
+    skip: int = 0
+    limit: int = 10
+    creation_year: int | None = None
+
+    @field_validator("creation_year")
+    def validate_year(cls, v):
+        current_year = date.today().year
+        if v is not None and (v > current_year or v < 1900):
+            raise BadRequestException("Invalid year")
+        return v
